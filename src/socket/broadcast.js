f103cf4e973e45f683f2f7dc982ca4499e06e577
Jeet Singh <jeets@webmobtech.com>
2023-05-25 11:37:50 +0530

    broadcast util functions

--- src/socket/broadcast.js
+++ src/socket/broadcast.js
@@ -1,8 +1,29 @@
-const broadcastToConversation = async (socket, io, data, temp) => {
+const { Conversations, Members } = require("../config/db");
+const { connectedUsers } = require("./store");
+
+exports.broadcastToConversation = async (io, data, str, conversationId) => {
   try {
-    const emitToOtherSockets = [];
-    emitToOtherSockets.forEach((socketId) => {
-      io.to(socketId).emit(temp, data);
+    const conversation = await Conversations.findByPk(conversationId);
+    connectedUsers.get(conversation.sender_id)?.forEach((socketId) => {
+      io.to(socketId).emit(str, data);
+    });
+    connectedUsers.get(conversation.receiver_id)?.forEach((socketId) => {
+      io.to(socketId).emit(str, data);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.broadcastToGroup = async (io, data, str, groupId) => {
+  try {
+    const members = await Members.findAll({
+      where: { group_id: groupId },
+    });
+    members.forEach((member) => {
+      connectedUsers.get(member.user_id)?.forEach((socketId) => {
+        io.to(socketId).emit(str, data);
+      });
     });
   } catch (error) {
     console.log(error);
